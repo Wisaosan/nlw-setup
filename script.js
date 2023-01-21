ced2009ac8c489c3f0184106a1893dc2e62c9c24
unknown <william.747@hotmal.com>
2023-01-21 13:36:06 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("NLWSetuo@habits", JSON.stringify(nlwSetup.data))
+}
+//const data = {
+//  run: ["01-01", "01-02", "01-06"],
+//  water: ["01-04", "01-05"],
+//  food: ["01-01", "01-03"],
+//}
+
+const data = JSON.parse(localStorage.getItem("NLWSetuo@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
